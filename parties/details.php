ce10034e67702815fc09d08e893966878c564a71
AntoineNeret <146841880+AntoineNeret@users.noreply.github.com>
2024-02-19 15:34:23 +0100

    Projet-cinema "fini"
    affichage de films et détail des films

--- parties/details.php
+++ parties/details.php
@@ -1,32 +1,96 @@
-<?php $id = null;
-$titre = null;
-$resume = null;
-$date_sortie = null;
-$pays = null;
-$image = null;
-$heure = null;
-$minutes = null;
-if (isset($_GET["id"]) && isset($_GET["titre"]) && isset($_GET["resume"]) && isset($_GET["date_sortie"]) && isset($_GET["pays"])) {
+<?php
+$id = null;
+if (isset($_GET["id"])) {
     $id = $_GET["id"];
-    $titre = $_GET["titre"];
-    $resume = $_GET["resume"];
-    $date_sortie = $_GET["date_sortie"];
-    $pays = $_GET["pays"];
-    $image = $_GET["image"];
-    $heure = $_GET["heure"];
-    $minutes = $_GET["minutes"];
-} ?>
+}
+/**
+ * @var PDO $connexion
+ */
+require "../config/db-config.php";
+require "../config/fonctions.php";
+
+$requete = $connexion->prepare("SELECT * FROM film WHERE id=$id ORDER BY titre");
+
+$requete->execute();
+
+$details = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($details as $detail):
+    $id = $detail["id"];
+    $titre = $detail["titre"];
+    $duree = $detail["duree"];
+    $resume = $detail["resume"];
+    $date_sortie = $detail["date_sortie"];
+    $pays = $detail["pays"];
+    $image = $detail["image"];
+
+    $minutes = $duree%60;
+    $heure = floor($duree/60);
+endforeach;
+$date = dateToFrench( $date_sortie ,"l j F Y");
+
+if ($heure>1){
+    $affichageHeure = "heures";
+}else{
+    $affichageHeure = "heure";
+}
+
+if ($minutes>1){
+    $affichageMinutes = "minutes";
+}elseif ($minutes==0){
+    $affichageMinutes = "";
+}else{
+    $affichageMinutes = "minute";
+}
+
+?>
 
 <!doctype html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.2/dist/sandstone/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.2/dist/lux/bootstrap.min.css" rel="stylesheet">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Les cinémas Haumont | Détail du film <?=$titre?></title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand fs-2 fw-bold" href="index.php">Les cinémas Haumont</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-current="page" aria-disabled="true">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true">Pricing</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container d-flex mt-5"><?=
+
+
+    "<img src='$image' class='w-25 ' alt='Image de $titre'>
+    <div class='ps-4'>
+    <p class='fw-bold fs-3 row'>$titre</p>
+    <p class='row'> est sorti le $date</p>
+    <p class='row'> dure $heure $affichageHeure et $minutes $affichageMinutes</p>
+    <p class='row fs-4 pt-2'> Synopsis</p>
+    <p class='row'>$resume</p>
+
+    </div>";
+    ?></>
+
+</body>
--- parties/index.php
+++ parties/index.php
@@ -3,6 +3,7 @@
  * @var PDO $connexion
  */
 require "../config/db-config.php";
+require "../config/fonctions.php";
 
 $requete = $connexion->prepare("SELECT * FROM film ORDER BY titre");
 
@@ -16,17 +17,41 @@ $films = $requete->fetchAll(PDO::FETCH_ASSOC);
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <link href="../css/style.css" rel="stylesheet">
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.2/dist/sandstone/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.2/dist/lux/bootstrap.min.css" rel="stylesheet">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Les cinémas Haumont | Liste de nos films</title>
 </head>
-<body class="bg-dark-subtle">
+<body class="bg-body-secondary ">
+
+<nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand fs-2 fw-bold" href="index.php">Les cinémas Haumont</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-current="page" aria-disabled="true">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true">Pricing</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<h1 class="mt-3 ms-3">Liste de nos films</h1>
 <?php
-echo "<div class='row row-cols-1 row-cols-md-2 g-4'>";
+echo "<div class='row row-cols-1 row-cols-md-2 g-4 m-3'>";
 foreach ($films as $film):
     $id = $film["id"];
     $titre = $film["titre"];
@@ -36,16 +61,17 @@ foreach ($films as $film):
     $pays = $film["pays"];
     $image = $film["image"];
 
+//    $image = "https://placehold.co/800/gray/white?text=".$film["titre"]."&font=roboto";
     $minutes = $duree%60;
     $heure = floor($duree/60);
 
     echo "
-                <div class='card border border-primary col-lg-6 bg-black text-light mx-auto' style='width: 18rem;'>
-                    <img src='$image' class='card-img-top' alt='Image de $titre'>
+                <div class='card border border-primary col-lg-6 bg-black text-light mx-auto ' style='width: 18rem;'>
+                    <img src='$image' class='card-img-top mt-2 rounded' alt='Image de $titre'>
                     <div class='card-body'>
                         <h5 class='card-title'>$titre</h5>
                         <p class='card-text'>Le film dure $heure h et $minutes m</p>
-                        <a class='btn btn-secondary' href='details.php?id=$id&titre=$titre&resume=$resume&date_sortie=$date_sortie&pays=$pays&image=$image&heure=$heure&minutes=$minutes'>Voir le détail</a>
+                        <a class='btn btn-secondary' href='details.php?id=$id'>Voir le détail</a>
                         </div>
                 </div>";
 endforeach;
